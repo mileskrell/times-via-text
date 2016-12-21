3bf998bd13c9aec74991e05fee6e8e185b82c68a
Miles Krell <noreply@mileskrell.com>
2016-12-20 21:44:20 -0600

    Separate app into two activities and add Google Maps stuff

--- app/src/main/java/com/example/mmkrell/timesviatext/MainActivity.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/MainActivity.java
@@ -1,71 +1,26 @@
 package com.example.mmkrell.timesviatext;
 
-import android.content.res.AssetManager;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Scanner;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    TextView textView2;
+    Button buttonOpenMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
-        textView2 = (TextView) findViewById(R.id.textView2);
+        buttonOpenMap = (Button) findViewById(R.id.buttonOpenMap);
 
-        ArrayList<Stop> stops = new ArrayList<Stop>();
-
-        AssetManager assetManager = getApplicationContext().getAssets();
-
-        Scanner outerScanner = null;
-        try {
-            outerScanner = new Scanner(assetManager.open("message.txt"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        outerScanner.nextLine(); // Skip the first line; it's just the legend
-        while (outerScanner.hasNextLine()) {
-            Scanner innerScanner = new Scanner(outerScanner.nextLine()).useDelimiter(",");
-            int currentStopId = innerScanner.nextInt();
-            int currentStopCode = innerScanner.nextInt();
-            String currentStopName = innerScanner.next();
-
-            String currentStopDesc;
-            String currentToken = innerScanner.next();
-            if (currentToken.equals("\"\"")) { // If stop_desc is "empty" (it has 2 sets of quotes), just move on
-                currentStopDesc = "";
-            } else {
-                currentStopDesc = currentToken + "," + innerScanner.next() + "," + innerScanner.next(); // Otherwise, use it and the following 2 tokens for the description
+        buttonOpenMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MapActivity.class));
             }
-
-            double currentStopLat = innerScanner.nextDouble();
-            double currentStopLon = innerScanner.nextDouble();
-            int currentLocationType = innerScanner.nextInt();
-
-            int currentParentStation;
-            if (innerScanner.hasNextInt()) { // If parent_station isn't empty, get its value
-                currentParentStation = innerScanner.nextInt();
-            } else {
-                currentParentStation = -1; // Otherwise, use -1 and move on
-                innerScanner.next();
-            }
-
-            int currentWheelchairBoarding = innerScanner.nextInt();
-            stops.add(new Stop(currentStopId, currentStopCode, currentStopName, currentStopDesc, currentStopLat, currentStopLon, currentLocationType, currentParentStation, currentWheelchairBoarding));
-        }
-        textView.setText(stops.get(0).getStopName() + " " + stops.get(0).getStopLat());
-        textView2.setText(stops.get(1).getStopName() + " " + stops.get(1).getStopLat());
+        });
     }
 }
---
+++ app/src/main/java/com/example/mmkrell/timesviatext/MapActivity.java
@@ -0,0 +1,84 @@
+package com.example.mmkrell.timesviatext;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    ArrayList<Stop> stops; // TODO: 12/20/16  This uses a huge amount of RAM. Learn how to use databases (or maybe just use arrays or something)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+
+        stops = new ArrayList<Stop>();
+
+        AssetManager assetManager = getApplicationContext().getAssets();
+
+        Scanner outerScanner = null;
+        try {
+            outerScanner = new Scanner(assetManager.open("message.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        outerScanner.nextLine(); // Skip the first line; it's just the legend
+        while (outerScanner.hasNextLine()) {
+            Scanner innerScanner = new Scanner(outerScanner.nextLine()).useDelimiter(",");
+            int currentStopId = innerScanner.nextInt();
+            int currentStopCode = innerScanner.nextInt();
+            String currentStopName = innerScanner.next();
+
+            String currentStopDesc;
+            String currentToken = innerScanner.next();
+            if (currentToken.equals("\"\"")) { // If stop_desc is "empty" (it has 2 sets of quotes), just move on
+                currentStopDesc = "";
+            } else {
+                currentStopDesc = currentToken + "," + innerScanner.next() + "," + innerScanner.next(); // Otherwise, use it and the following 2 tokens for the description
+            }
+
+            double currentStopLat = innerScanner.nextDouble();
+            double currentStopLon = innerScanner.nextDouble();
+            int currentLocationType = innerScanner.nextInt();
+
+            int currentParentStation;
+            if (innerScanner.hasNextInt()) { // If parent_station isn't empty, get its value
+                currentParentStation = innerScanner.nextInt();
+            } else {
+                currentParentStation = -1; // Otherwise, use -1 and move on
+                innerScanner.next();
+            }
+
+            int currentWheelchairBoarding = innerScanner.nextInt();
+            stops.add(new Stop(currentStopId, currentStopCode, currentStopName, currentStopDesc, currentStopLat, currentStopLon, currentLocationType, currentParentStation, currentWheelchairBoarding));
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        for (Stop stop : stops) {
+            LatLng currentStop = new LatLng(stop.getStopLat(), stop.getStopLon());
+            googleMap.addMarker(new MarkerOptions().position(currentStop).title(String.valueOf(stop.getStopCode())));
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(stops.get(0).getStopLat(), stops.get(0).getStopLon())));
+    }
+}
