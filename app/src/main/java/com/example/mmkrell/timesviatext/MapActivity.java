79da058ad7b4ca590a5a0a0313950b8ab3fec5c2
Miles Krell <noreply@mileskrell.com>
2016-12-28 20:51:05 -0600

    Remove a lot of unused import statements

--- app/src/main/java/com/example/mmkrell/timesviatext/MapActivity.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/MapActivity.java
@@ -1,17 +1,11 @@
 package com.example.mmkrell.timesviatext;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
