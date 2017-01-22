ede555d9fd044c2e30d438f170d821444856b03e
Miles Krell <mileskrell@gmail.com>
2017-01-21 20:20:09 -0600

    Make StopFragment display stop code and stop name. Also move ItemizedIconOverlay creation to onCreate().

--- app/src/main/java/com/example/mmkrell/timesviatext/StopFragment.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/StopFragment.java
@@ -5,23 +5,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class StopFragment extends Fragment {
+
+    private int stopCode;
+    private String stopName;
+
+    TextView stopFragmentTextView;
 
     public StopFragment() {
 
     }
 
-    public static StopFragment newInstance(String stopName) {
+    public static StopFragment newInstance(int stopCode, String stopName) {
         StopFragment stopFragment = new StopFragment();
         Bundle args = new Bundle();
-        args.putString("stop_name", stopName);
+        args.putInt("stopCode", stopCode);
+        args.putString("stopName", stopName);
         stopFragment.setArguments(args);
         return stopFragment;
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            stopCode = getArguments().getInt("stopCode");
+            stopName = getArguments().getString("stopName");
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stop, container, false);
+        View v = inflater.inflate(R.layout.fragment_stop, container, false);
+        stopFragmentTextView = (TextView) v.findViewById(R.id.stopFragmentTextView);
+        stopFragmentTextView.setText(stopName + "\n" + stopCode);
+        return v;
     }
 }
