1a37a3e2d6a2980523f6ef367d4c9ffdbd8684e2
Miles Krell <mileskrell@gmail.com>
2017-02-19 18:09:19 -0600

    Renamed GTFSHelper to CTAHelper

--- app/src/main/java/com/example/mmkrell/timesviatext/CTAHelper.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/CTAHelper.java
@@ -4,12 +4,12 @@ import android.content.Context;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
-class GTFSHelper extends SQLiteAssetHelper {
+class CTAHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "cta.db";
     private static final int DATABASE_VERSION = 1;
 
-    GTFSHelper(Context context) {
+    CTAHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 }
