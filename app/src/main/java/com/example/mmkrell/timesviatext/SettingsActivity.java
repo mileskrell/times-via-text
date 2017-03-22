ae3486b68cbe337c81c8c6fe763a01d302bf4d69
Miles Krell <noreply@mileskrell.com>
2017-03-21 22:35:03 -0500

    Added an Activity for settings

---
+++ app/src/main/java/com/example/mmkrell/timesviatext/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.example.mmkrell.timesviatext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, SettingsFragment.newInstance()).commit();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
