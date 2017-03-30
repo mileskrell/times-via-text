74a7a3e18712898d7c7c0ef2b086407f8355b1b5
Miles Krell <mileskrell@gmail.com>
2017-03-29 21:23:09 -0500

    Added AboutActivity

---
+++ app/src/main/java/com/example/mmkrell/timesviatext/AboutActivity.java
@@ -0,0 +1,27 @@
+package com.example.mmkrell.timesviatext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
