eb35c49d32ad7b5b8ad06f7e802aad2c5d2bcdad
Miles Krell <mileskrell@gmail.com>
2017-04-14 13:20:44 -0500

    Add a "favorite" button to StopFragment. Favorites are stored as a Set of stop codes in SharedPreferences.

--- app/src/main/java/com/example/mmkrell/timesviatext/StopFragment.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/StopFragment.java
@@ -1,21 +1,32 @@
 package com.example.mmkrell.timesviatext;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.HashSet;
 
 public class StopFragment extends Fragment {
 
     private int stopCode;
     private String stopName;
     private String stopDesc;
+
+    private boolean checked;
+    private SharedPreferences sharedPreferences;
+    private HashSet<String> favorites;
 
     public StopFragment() {
         // Required empty public constructor
@@ -44,9 +55,14 @@ public class StopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_stop, container, false);
+
         TextView textViewName = (TextView) v.findViewById(R.id.stop_fragment_text_view_name);
         TextView textViewDirection = (TextView) v.findViewById(R.id.stop_fragment_text_view_direction);
+        final ImageView buttonFavorite = (ImageView) v.findViewById(R.id.stop_fragment_button_favorite);
         Button buttonWriteText = (Button) v.findViewById(R.id.stop_fragment_button_write_text);
+
+        final Drawable uncheckedFavorite =  ContextCompat.getDrawable(getContext(), R.drawable.ic_favorite_border_red_24dp);
+        final Drawable checkedFavorite = ContextCompat.getDrawable(getContext(), R.drawable.ic_favorite_red_24dp);
 
         textViewName.setText(stopName);
 
@@ -58,6 +74,38 @@ public class StopFragment extends Fragment {
             String stopDirection = stopDesc.substring(startPos, endPos);
             textViewDirection.setText(stopDirection);
         }
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        favorites = new HashSet<>(sharedPreferences.getStringSet("favorites", new HashSet<String>()));
+
+        // If this stop is a favorite, make the heart filled to represent that
+        if (favorites.contains(String.valueOf(stopCode))) {
+            checked = true;
+            buttonFavorite.setImageDrawable(checkedFavorite);
+        }
+
+        buttonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Flip the "checked" boolean
+                checked = ! checked;
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                if (checked) {
+                    // Set image to filled heart and add stop to favorites
+                    buttonFavorite.setImageDrawable(checkedFavorite);
+                    favorites.add(String.valueOf(stopCode));
+                } else {
+                    // Set image to empty heart and remove stop from favorites
+                    buttonFavorite.setImageDrawable(uncheckedFavorite);
+                    favorites.remove(String.valueOf(stopCode));
+                }
+
+                editor.putStringSet("favorites", favorites);
+                editor.apply();
+            }
+        });
 
         buttonWriteText.setOnClickListener(new View.OnClickListener() {
             @Override
