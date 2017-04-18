da3bbee27811eb6473ff8f1a270000bfe5ac6ae5
Miles Krell <mileskrell@gmail.com>
2017-04-17 23:08:43 -0500

    Fixed order of import statements

--- app/src/main/java/com/example/mmkrell/timesviatext/AboutActivity.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/AboutActivity.java
@@ -1,7 +1,7 @@
 package com.example.mmkrell.timesviatext;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
--- app/src/main/java/com/example/mmkrell/timesviatext/DownloadMapTilesActivity.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/DownloadMapTilesActivity.java
@@ -1,9 +1,9 @@
 package com.example.mmkrell.timesviatext;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
