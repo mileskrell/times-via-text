a30f8aeaf63dd6e1eb644e4e8bfe79248de0248c
Miles Krell <mileskrell@gmail.com>
2017-04-27 11:06:33 -0500

    Removed all tile downloading stuff for now; it will have to be remade as a Service some day

--- app/src/main/java/com/example/mmkrell/timesviatext/NavigationBarActivity.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/NavigationBarActivity.java
@@ -1,9 +1,7 @@
 package com.example.mmkrell.timesviatext;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
@@ -74,12 +72,6 @@ public class NavigationBarActivity extends AppCompatActivity {
 
         // Select the map fragment at startup
         bottomNavigationView.setSelectedItemId(R.id.navigation_map);
-
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
-        // Have the user download map tiles, if they haven't before
-        if (! sharedPreferences.getBoolean("has_downloaded_tiles", false))
-            startActivity(new Intent(this, DownloadMapTilesActivity.class));
     }
 
     @Override
@@ -91,9 +83,6 @@ public class NavigationBarActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.options_menu_download_tiles:
-                startActivity(new Intent(this, DownloadMapTilesActivity.class));
-                return true;
             case R.id.options_menu_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
                 return true;
