bd457c21a1fa555f930abfb1f540ac35d62f5501
Miles Krell <mileskrell@gmail.com>
2017-05-04 20:59:00 -0500

    Created the first third of RoutesFragment (a RecyclerView with a list of routes)

---
+++ app/src/main/java/com/example/mmkrell/timesviatext/RoutesAdapter.java
@@ -0,0 +1,68 @@
+package com.example.mmkrell.timesviatext;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+class RoutesAdapter extends RecyclerView.Adapter<RoutesAdapter.ViewHolder> {
+
+    private final ArrayList<String> routes;
+    private final SQLiteDatabase database;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewRouteID;
+        TextView textViewRouteName;
+
+        View itemView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            textViewRouteName = (TextView) itemView.findViewById(R.id.favorite_text_view_route_name);
+            textViewRouteID = (TextView) itemView.findViewById(R.id.favorite_text_view_route_id);
+        }
+    }
+
+    RoutesAdapter(Context context) {
+        database = new CTAHelper(context).getReadableDatabase();
+        routes = new ArrayList<>();
+        Cursor query = database.rawQuery("SELECT route_id FROM routes", null);
+        while (query.moveToNext()) {
+            routes.add(query.getString(0));
+        }
+        query.close();
+    }
+
+    @Override
+    public RoutesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_route, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Cursor query = database.rawQuery("SELECT route_long_name FROM routes WHERE route_id = ?",
+                new String[] {routes.get(position)});
+        query.moveToNext();
+
+        holder.textViewRouteID.setText(routes.get(position));
+        holder.textViewRouteName.setText(query.getString(0));
+
+        query.close();
+    }
+
+    @Override
+    public int getItemCount() {
+        return routes.size();
+    }
+}
--- app/src/main/java/com/example/mmkrell/timesviatext/RoutesFragment.java
+++ app/src/main/java/com/example/mmkrell/timesviatext/RoutesFragment.java
@@ -2,6 +2,8 @@ package com.example.mmkrell.timesviatext;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +20,15 @@ public class RoutesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_routes, container, false);
-    }
 
+        View v = inflater.inflate(R.layout.fragment_routes, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) v.findViewById(R.id.routes_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerView.setAdapter(new RoutesAdapter(getContext()));
+
+        return v;
+    }
 }
